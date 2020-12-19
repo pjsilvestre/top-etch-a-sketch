582131a97060df6aed6a79a7b570cb7db4adcce8
Patrick Silvestre <patrickjohnsilvestre@gmail.com>
2020-12-19 04:49:20 -0800

    Allow for variable number of squares per row

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
 /**
- * Adds grid squares to the document body
+ * Creates the grid and appends it to the document's body
+ * @param {Number} squaresPerRow The desired number of squares per row
  */
-function addGridSquares() {
-  const gridContainer = document.createElement('div');
-  gridContainer.id = 'grid-container';
+function addGridSquares(squaresPerRow) {
+  if (typeof squaresPerRow !== 'number') {
+    console.trace(`addGridSquares invoked with non-number argument: ` +
+      `${squaresPerRow}`);
+    return;
+  }
 
-  // TODO variable grid size
-  for (let i = 0; i < 256; i++) {
+  const gridContainer = document.createElement('div');
+
+  const gridStyle = {
+    display: 'grid',
+    gridTemplateRows: `repeat(${squaresPerRow}, ${960 / squaresPerRow}px)`,
+    gridTemplateColumns: `repeat(${squaresPerRow}, ${960 / squaresPerRow}px)`,
+  };
+
+  Object.assign(gridContainer.style, gridStyle);
+
+  for (let i = 0; i < squaresPerRow * squaresPerRow; i++) {
     const square = document.createElement('div');
     square.classList.add('grid-square');
 
@@ -20,4 +33,5 @@ function addGridSquares() {
   document.body.appendChild(gridContainer);
 }
 
-addGridSquares();
+const defaultSquaresPerRow = 16;
+addGridSquares(defaultSquaresPerRow);
